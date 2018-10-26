839aa1d766b43eef2dfde900fa2fc71281273bd3
bao <547799195@qq.com>
2018-10-26 18:36:34 +0800

    购物车功能

---
+++ js/gwc.js
@@ -0,0 +1,71 @@
+//获取相对元素
+var byPrice = $('#by-price');
+var byBtnR = $('#by-btn-reduce');//
+var byBtnA = $('#by-btn-add');
+var byInputN = $('#by-input-num');
+var byHj = $('#by-hj');
+$(function(){
+     byHj.text(byPrice.text() * byInputN.val())
+})
+//按钮减且赋值到价格
+byBtnR.click(function () {
+    if (byInputN.val() <= 1) {
+
+        byBtnR.attr('disabled', 'disabled');
+    }
+})
+
+byInputN.change(function(){
+    byHj.text(byPrice.text() * byInputN.val());
+})
+//全选按钮
+function checkAll(){
+    $('#by-checkAll').change(function(){
+        console.log($(this)[0].checked);
+        if(!$(this)[0].checked){
+            $('.by-check').each(function(index,val){
+                val.checked=false;
+            })
+        }else{
+            $('.by-check').each(function(index,val){
+                val.checked=true;
+            })
+        }
+    })
+}
+//单选按钮
+function check(){
+    $('body').on('change','.by-check',function(){
+        $('.by-check').each(function(index,val){
+          console.log(JSON.stringify(val.checked));
+
+          
+        })
+    })
+    
+}
+//合计按钮
+$('.by-jz-btn').click(function(){
+    var chexck  = $('#by-check-01')[0];
+    console.log(chexck.checked);
+    
+    if(chexck.checked &&  byInputN.val()!=0){
+        alert('成功支付...'+byHj.text());
+        // $.ajax({
+        //     type:'GET',
+        //     datatype:'json',
+        //     data:{
+        //         "by-zj":byHj.text(byPrice.text() * byInputN.val())
+        //     },
+        //     url:'',
+        //     success:function(data){
+        //         console.log(data.data)
+        //     }
+        // })
+    }else{
+        alert('请选择商品...')
+    }
+})
+
+checkAll();
+check();
